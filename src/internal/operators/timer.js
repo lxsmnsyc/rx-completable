a9f92cd4f8f35d9f866341ffd33a278f1f318e06
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:51:05 +0800

    Create timer.js

---
+++ src/internal/operators/timer.js
@@ -0,0 +1,40 @@
+import AbortController from 'abort-controller';
+import Completable from '../../completable';
+import { cleanObserver } from '../utils';
+import error from './error';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onComplete, onSubscribe } = cleanObserver(observer);
+
+
+  const controller = new AbortController();
+
+  const { signal } = controller;
+
+  onSubscribe(controller);
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const timeout = setTimeout(onComplete, this.amount);
+
+  signal.addEventListener('abort', () => {
+    clearTimeout(timeout);
+  });
+}
+/**
+ * @ignore
+ */
+export default (amount) => {
+  if (typeof amount !== 'number') {
+    return error(new Error('Completable.timer: "amount" is not a number.'));
+  }
+  const completable = new Completable();
+  completable.amount = amount;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
