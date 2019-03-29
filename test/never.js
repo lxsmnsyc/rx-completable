94bd0ee2f22b23fee953003947950d2a7ae2467f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:34:06 +0800

    Create never.js

---
+++ test/never.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#never', () => {
+    /**
+     *
+     */
+    it('should create a Completable', () => {
+      const completable = Completable.never();
+      assert(completable instanceof Completable);
+    });
+    /**
+     *
+     */
+    it('should not signal.', () => {
+      const completable = Completable.never();
+      completable.subscribe(
+        () => done(false),
+        () => done(false),
+      );
+    });
+  });
+});
