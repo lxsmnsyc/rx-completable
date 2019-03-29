5d081e28a5090bd8065dbee19e38b210a13e1b6e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:21:11 +0800

    Create create.js

---
+++ src/internal/operators/create.js
@@ -0,0 +1,41 @@
+import AbortController from 'abort-controller';
+import {
+  onErrorHandler, onCompleteHandler, cleanObserver,
+} from '../utils';
+import Completable from '../../completable';
+import error from './error';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onComplete, onError, onSubscribe } = cleanObserver(observer);
+
+  const emitter = new AbortController();
+  emitter.onComplete = onCompleteHandler.bind(this);
+  emitter.onError = onErrorHandler.bind(this);
+
+  this.controller = emitter;
+  this.onComplete = onComplete;
+  this.onError = onError;
+
+  onSubscribe(emitter);
+
+  try {
+    this.subscriber(emitter);
+  } catch (ex) {
+    emitter.onError(ex);
+  }
+}
+/**
+ * @ignore
+ */
+export default (subscriber) => {
+  if (typeof subscriber !== 'function') {
+    return error(new Error('Completable.create: There are no subscribers.'));
+  }
+  const single = new Completable();
+  single.subscriber = subscriber;
+  single.subscribeActual = subscribeActual.bind(single);
+  return single;
+};
