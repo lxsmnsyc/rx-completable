f89455df2279b5d36bc994e5fac1ac46dfe27882
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:34:16 +0800

    Create then.js

---
+++ test/then.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#then', () => {
+    /**
+     *
+     */
+    it('should create a Promise', () => {
+      const completable = Completable.complete().then(x => x, x => x);
+      assert(completable instanceof Promise);
+    });
+  });
+});
