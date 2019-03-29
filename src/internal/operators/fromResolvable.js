da930ae3f815c47839bb96cac20a30291f4a5483
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:33:35 +0800

    Create fromResolvable.js

---
+++ src/internal/operators/fromResolvable.js
@@ -0,0 +1,37 @@
+import AbortController from 'abort-controller';
+import { onErrorHandler, onCompleteHandler } from '../utils';
+import Completable from '../../completable';
+import { error } from '../operators';
+
+function subscribeActual(observer) {
+  const { onComplete, onError, onSubscribe } = observer;
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  if (controller.signal.aborted) {
+    return;
+  }
+
+  this.controller = controller;
+  this.onComplete = onComplete;
+  this.onError = onError;
+
+  const resolve = onCompleteHandler.bind(this);
+  const reject = onErrorHandler.bind(this);
+
+  this.subscriber(resolve, reject);
+}
+/**
+ * @ignore
+ */
+export default (subscriber) => {
+  if (typeof subscriber !== 'function') {
+    return error(new Error('Completable.fromResolvable: expects a function.'));
+  }
+  const completable = new Completable();
+  completable.subscriber = subscriber;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
