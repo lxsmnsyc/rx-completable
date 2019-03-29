e57804005e0e19f319f3f239d207da099efab69c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:33:27 +0800

    Create defer.js

---
+++ test/defer.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#defer', () => {
+    /**
+     *
+     */
+    it('should create a Completable', () => {
+      const completable = Completable.defer(() => Completable.complete());
+
+      assert(completable instanceof Completable);
+    });
+    /**
+     *
+     */
+    it('should succeed with the given value.', (done) => {
+      const completable = Completable.defer(() => Completable.complete());
+
+      completable.subscribe(
+        x => (x === 'Hello World' ? done() : done(false)),
+        e => done(e),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if callable returns a non-Completable', (done) => {
+      const completable = Completable.defer(() => {});
+
+      completable.subscribe(
+        () => done(false),
+        () => done(),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if callable throws an error', (done) => {
+      const completable = Completable.defer(() => {
+        throw new Error('Expected');
+      });
+
+      completable.subscribe(
+        () => done(false),
+        e => (e instanceof Error ? done() : done(false)),
+      );
+    });
+  });
+});
