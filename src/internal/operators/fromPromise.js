ecc459358268596e369f1c10991cbf37a57778f8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:32:43 +0800

    Create fromPromise.js

---
+++ src/internal/operators/fromPromise.js
@@ -0,0 +1,41 @@
+import AbortController from 'abort-controller';
+import Completable from '../../completable';
+import {
+  isPromise, onCompleteHandler, onErrorHandler,
+} from '../utils';
+import error from './error';
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onComplete, onError, onSubscribe } = observer;
+
+  const controller = new AbortController();
+
+  onSubscribe(controller);
+
+  if (controller.signal.aborted) {
+    return;
+  }
+
+  this.controller = controller;
+  this.onComplete = onComplete;
+  this.onError = onError;
+
+  this.promise.then(
+    onCompleteHandler.bind(this),
+    onErrorHandler.bind(this),
+  );
+}
+/**
+ * @ignore
+ */
+export default (promise) => {
+  if (!isPromise(promise)) {
+    return error(new Error('Completable.fromPromise: expects a Promise-like value.'));
+  }
+  const completable = new Completable();
+  completable.promise = promise;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
