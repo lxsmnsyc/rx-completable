c5e5334ddcfdefe2b6d8ba3844e3ea6d4246cb1a
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:23:04 +0800

    Create lift.js

---
+++ src/internal/operators/lift.js
@@ -0,0 +1,37 @@
+import Completable from '../../completable';
+import { isObserver, immediateError } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  let result;
+
+  try {
+    result = this.operator(observer);
+
+    if (!isObserver(result)) {
+      throw new Error('Completable.lift: operator returned a non-Observer.');
+    }
+  } catch (e) {
+    immediateError(observer, e);
+    return;
+  }
+
+  this.source.subscribeWith(result);
+}
+
+/**
+ * @ignore
+ */
+export default (source, operator) => {
+  if (typeof operator !== 'function') {
+    return source;
+  }
+
+  const completable = new Completable();
+  completable.source = source;
+  completable.operator = operator;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
