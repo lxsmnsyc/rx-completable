ff41cab56ff871a2e94d037674e969129f3cc204
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 14:41:42 +0800

    Create doOnSubscribe.js

---
+++ src/internal/operators/doOnSubscribe.js
@@ -0,0 +1,33 @@
+import Completable from '../../completable';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onSubscribe, onComplete, onError } = observer;
+
+  const { source, callable } = this;
+
+  source.subscribeWith({
+    onSubscribe(d) {
+      callable(d);
+      onSubscribe(d);
+    },
+    onComplete,
+    onError,
+  });
+}
+
+/**
+ * @ignore
+ */
+export default (source, callable) => {
+  if (typeof callable !== 'function') {
+    return source;
+  }
+  const completable = new Completable();
+  completable.source = source;
+  completable.callable = callable;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
