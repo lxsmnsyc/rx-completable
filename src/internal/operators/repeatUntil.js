c6c5f50f52332def5d0ccc39b718e578a329c0e2
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:12:44 +0800

    Create repeatUntil.js

---
+++ src/internal/operators/repeatUntil.js
@@ -0,0 +1,62 @@
+import AbortController from 'abort-controller';
+import Completable from '../../completable';
+import { cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onSubscribe, onError } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  const { signal } = controller;
+
+  onSubscribe(controller);
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, predicate } = this;
+
+  const sub = () => {
+    if (signal.aborted) {
+      return;
+    }
+
+    source.subscribeWith({
+      onSubscribe(ac) {
+        signal.addEventListener('abort', () => ac.abort());
+      },
+      onComplete() {
+        if (typeof predicate === 'function') {
+          const result = predicate();
+
+          if (result) {
+            sub();
+          }
+        } else {
+          sub();
+        }
+      },
+      onError(x) {
+        onError(x);
+        controller.abort();
+      },
+    });
+  };
+
+  sub();
+}
+
+/**
+ * @ignore
+ */
+export default (source, predicate) => {
+  const completable = new Completable();
+  completable.source = source;
+  completable.predicate = predicate;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
