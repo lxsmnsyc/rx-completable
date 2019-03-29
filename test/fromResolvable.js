cb8e64a7de4a2159ac602f77fb1510a72124f343
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:34:00 +0800

    Create fromResolvable.js

---
+++ test/fromResolvable.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#fromResolvable', () => {
+    /**
+     *
+     */
+    it('should create a Completable', () => {
+      const completable = Completable.fromResolvable(res => res('Hello World'));
+      assert(completable instanceof Completable);
+    });
+    /**
+     *
+     */
+    it('should succeed with the given value.', (done) => {
+      const completable = Completable.fromResolvable(res => res('Hello World'));
+
+      completable.subscribe(
+        x => (x === 'Hello World' ? done() : done(false)),
+        e => done(e),
+      );
+    });
+    /**
+     *
+     */
+    it('should signal error if the given value is not a function', (done) => {
+      const completable = Completable.fromResolvable();
+
+      completable.subscribe(
+        () => done(false),
+        () => done(),
+      );
+    });
+  });
+});
