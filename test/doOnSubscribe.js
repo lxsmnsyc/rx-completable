84e58d432a9e9437752f57960bef091d8f531924
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:33:49 +0800

    Create doOnSubscribe.js

---
+++ test/doOnSubscribe.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#doOnSubscribe', () => {
+    /**
+     *
+     */
+    it('should create a Completable', () => {
+      const completable = Completable.complete().doOnSubscribe(() => {});
+      assert(completable instanceof Completable);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if no function is passed', () => {
+      const source = Completable.complete();
+      const completable = source.doOnSubscribe();
+      assert(source === completable);
+    });
+    /**
+     *
+     */
+    it('should be called before actual subscription.', (done) => {
+      let called;
+      const completable = Completable.complete().doOnSubscribe(() => { called = true; });
+      completable.subscribeWith({
+        onSubscribe() {
+          if (called) {
+            done();
+          } else {
+            done(false);
+          }
+        },
+      });
+    });
+  });
+});
