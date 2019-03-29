0133e82aa6b0a880719400fbc34b3c903b4fd0a1
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 15:01:51 +0800

    Create retry.js

---
+++ src/internal/operators/retry.js
@@ -0,0 +1,68 @@
+import AbortController from 'abort-controller';
+import Completable from '../../completable';
+import { cleanObserver } from '../utils';
+
+/**
+ * @ignore
+ */
+function subscribeActual(observer) {
+  const { onSubscribe, onComplete, onError } = cleanObserver(observer);
+
+  const controller = new AbortController();
+
+  const { signal } = controller;
+
+  onSubscribe(controller);
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const { source, bipredicate } = this;
+
+  let retries = 0;
+
+  const sub = () => {
+    if (signal.aborted) {
+      return;
+    }
+    retries += 1;
+
+    source.subscribeWith({
+      onSubscribe(ac) {
+        signal.addEventListener('abort', () => ac.abort());
+      },
+      onComplete() {
+        onComplete();
+        controller.abort();
+      },
+      onError(x) {
+        if (typeof bipredicate === 'function') {
+          const result = bipredicate(retries, x);
+
+          if (result) {
+            sub();
+          } else {
+            onError(x);
+            controller.abort();
+          }
+        } else {
+          sub();
+        }
+      },
+    });
+  };
+
+  sub();
+}
+
+/**
+ * @ignore
+ */
+export default (source, bipredicate) => {
+  const completable = new Completable();
+  completable.source = source;
+  completable.bipredicate = bipredicate;
+  completable.subscribeActual = subscribeActual.bind(completable);
+  return completable;
+};
