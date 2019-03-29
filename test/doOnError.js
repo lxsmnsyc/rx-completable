cad59f40a33c78c9871dd4a2df3de3cfac3c99c1
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-29 18:33:44 +0800

    Create doOnError.js

---
+++ test/doOnError.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import Completable from '../src/completable';
+
+/**
+ *
+ */
+describe('Completable', () => {
+  /**
+   *
+   */
+  describe('#doOnError', () => {
+    /**
+     *
+     */
+    it('should create a Completable', () => {
+      const completable = Completable.complete().doOnError(() => {});
+      assert(completable instanceof Completable);
+    });
+    /**
+     *
+     */
+    it('should return the same instance if the method received a non-function parameter.', () => {
+      const source = Completable.complete();
+      const completable = source.doOnError();
+      assert(source === completable);
+    });
+    /**
+     *
+     */
+    it('should call the given function on error.', (done) => {
+      let called;
+      const source = Completable.error(new Error('Hello'));
+      const completable = source.doOnError(() => { called = true; });
+      completable.subscribe(
+        () => done(false),
+        () => called && done(),
+      );
+    });
+  });
+});
