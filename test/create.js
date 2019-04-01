5300b27b23fb8c7ee09da4d9c169ed53de70cb31
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:26:44 +0800

    Update create.js

--- test/create.js
+++ test/create.js
@@ -5,81 +5,76 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#create', () => {
   /**
    *
    */
-  describe('#create', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.create(e => e.onSuccess('Hello World'));
+  it('should create a Completable', () => {
+    const completable = Completable.create(e => e.onComplete('Hello World'));
 
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should signal error if the create received a non-function', (done) => {
-      const completable = Completable.create();
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should signal error if the create received a non-function', (done) => {
+    const completable = Completable.create();
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the emitter signals success with undefined value.', (done) => {
-      const completable = Completable.create(e => e.onSuccess() || e.onSuccess());
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the emitter signals complete.', (done) => {
+    const completable = Completable.create(e => e.onComplete() || e.onComplete());
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the emitter signals error with undefined value.', (done) => {
-      const completable = Completable.create(e => e.onError() || e.onError());
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the emitter signals error with undefined value.', (done) => {
+    const completable = Completable.create(e => e.onError() || e.onError());
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should be aborted successfully if emitter is aborted before any signal.', (done) => {
+    const completable = Completable.create((e) => {
+      setTimeout(e.onComplete, 100);
     });
-    /**
-     *
-     */
-    it('should be aborted successfully if emitter is aborted before any signal.', (done) => {
-      const completable = Completable.create((e) => {
-        setTimeout(e.onSuccess, 100, true);
-        e.abort();
-      });
 
-      const controller = completable.subscribe(
-        () => done(false),
-        () => done(false),
-      );
-      if (controller.signal.aborted) {
-        done();
-      }
+    const controller = completable.subscribe(
+      () => done(false),
+      () => done(false),
+    );
+    controller.abort();
+    if (controller.signal.aborted) {
+      done();
+    }
+  });
+  /**
+   *
+   */
+  it('should signal error if subscriber throws an error.', (done) => {
+    const completable = Completable.create(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should signal error if subscriber throws an error.', (done) => {
-      const completable = Completable.create(() => {
-        throw new Error('Expected');
-      });
 
-      completable.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(e)),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
