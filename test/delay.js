f32f1f0b60bcf140db8b695f7bddb4be1600e7ef
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:26:49 +0800

    Update delay.js

--- test/delay.js
+++ test/delay.js
@@ -5,75 +5,70 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#delay', () => {
   /**
    *
    */
-  describe('#delay', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.complete().delay(100);
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if the amount is not a number.', () => {
-      const source = Completable.complete();
-      const completable = source.delay();
-      assert(source === completable);
-    });
-    /**
-     *
-     */
-    it('should signal success with the given value.', (done) => {
-      const completable = Completable.complete().delay(100);
-      completable.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        x => done(x),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error with the given value.', (done) => {
-      const completable = Completable.error(new Error('Hello')).delay(100);
-      completable.subscribe(
-        x => done(x),
-        x => (x === 'Hello' ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should not signal success if aborted.', (done) => {
-      const source = Completable.complete().delay(100);
-      const controller = source.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+  it('should create a Completable', () => {
+    const completable = Completable.complete().delay(100);
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if the amount is not a number.', () => {
+    const source = Completable.complete();
+    const completable = source.delay();
+    assert(source === completable);
+  });
+  /**
+   *
+   */
+  it('should signal complete.', (done) => {
+    const completable = Completable.complete().delay(100);
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error with the given value.', (done) => {
+    const completable = Completable.error(new Error('Hello')).delay(100);
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should not signal complete if aborted.', (done) => {
+    const source = Completable.complete().delay(100);
+    const controller = source.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
-    /**
-     *
-     */
-    it('should not signal error if aborted.', (done) => {
-      const source = Completable.error(new Error('Hello')).delay(100);
-      const controller = source.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+    controller.abort();
+    if (controller.signal.aborted) {
+      done();
+    }
+  });
+  /**
+   *
+   */
+  it('should not signal error if aborted.', (done) => {
+    const source = Completable.error(new Error('Hello')).delay(100);
+    const controller = source.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
+    controller.abort();
+    if (controller.signal.aborted) {
+      done();
+    }
   });
 });
