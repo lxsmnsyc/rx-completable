5f8f321907c54934c90ea8d98c99189a002fc8f6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:09 +0800

    Update doOnTerminate.js

--- test/doOnTerminate.js
+++ test/doOnTerminate.js
@@ -5,49 +5,44 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#doOnTerminate', () => {
   /**
    *
    */
-  describe('#doOnTerminate', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.complete().doOnTerminate(() => {});
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if the method received a non-function parameter.', () => {
-      const source = Completable.complete();
-      const completable = source.doOnTerminate();
-      assert(source === completable);
-    });
-    /**
-     *
-     */
-    it('should call the given function on success.', (done) => {
-      let called;
-      const source = Completable.complete();
-      const completable = source.doOnTerminate(() => { called = true; });
-      completable.subscribe(
-        () => called && done(),
-        () => done(false),
-      );
-    });
-    /**
-     *
-     */
-    it('should call the given function on error.', (done) => {
-      let called;
-      const source = Completable.error(new Error('Hello'));
-      const completable = source.doOnTerminate(() => { called = true; });
-      completable.subscribe(
-        () => done(false),
-        () => called && done(),
-      );
-    });
+  it('should create a Completable', () => {
+    const completable = Completable.complete().doOnTerminate(() => {});
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if the method received a non-function parameter.', () => {
+    const source = Completable.complete();
+    const completable = source.doOnTerminate();
+    assert(source === completable);
+  });
+  /**
+   *
+   */
+  it('should call the given function on complete.', (done) => {
+    let called;
+    const source = Completable.complete();
+    const completable = source.doOnTerminate(() => { called = true; });
+    completable.subscribe(
+      () => called && done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should call the given function on error.', (done) => {
+    let called;
+    const source = Completable.error(new Error('Hello'));
+    const completable = source.doOnTerminate(() => { called = true; });
+    completable.subscribe(
+      () => done(false),
+      () => called && done(),
+    );
   });
 });
