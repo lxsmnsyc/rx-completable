976a0d68443174965e895cbb3f19b022a80f4a89
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:46 +0800

    Update timer.js

--- test/timer.js
+++ test/timer.js
@@ -5,54 +5,49 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#timer', () => {
   /**
    *
    */
-  describe('#timer', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.timer(100);
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should signal error if amount is not a number.', (done) => {
-      const completable = Completable.timer();
+  it('should create a Completable', () => {
+    const completable = Completable.timer(100);
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should signal error if amount is not a number.', (done) => {
+    const completable = Completable.timer();
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal success 0', (done) => {
-      const completable = Completable.timer(100);
-      completable.subscribe(
-        x => (x === 0 ? done() : done(false)),
-        () => done(false),
-      );
-    });
-    /**
-     *
-     */
-    it('should not signal success if aborted.', (done) => {
-      const completable = Completable.timer(100);
-      const controller = completable.subscribe(
-        () => done(false),
-        () => done(false),
-      );
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal complete', (done) => {
+    const completable = Completable.timer(100);
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should not signal complete if aborted.', (done) => {
+    const completable = Completable.timer(100);
+    const controller = completable.subscribe(
+      () => done(false),
+      () => done(false),
+    );
 
 
-      controller.abort();
-      if (controller.signal.aborted) {
-        done();
-      }
-    });
+    controller.abort();
+    if (controller.signal.aborted) {
+      done();
+    }
   });
 });
