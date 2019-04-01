d9e37572ce32d33db6852280848410799ca999cc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:17 +0800

    Update fromResolvable.js

--- test/fromResolvable.js
+++ test/fromResolvable.js
@@ -5,39 +5,34 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#fromResolvable', () => {
   /**
    *
    */
-  describe('#fromResolvable', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.fromResolvable(res => res('Hello World'));
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should succeed with the given value.', (done) => {
-      const completable = Completable.fromResolvable(res => res('Hello World'));
+  it('should create a Completable', () => {
+    const completable = Completable.fromResolvable(res => res('Hello World'));
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should succeed with the given value.', (done) => {
+    const completable = Completable.fromResolvable(res => res('Hello World'));
 
-      completable.subscribe(
-        x => (x === 'Hello World' ? done() : done(false)),
-        e => done(e),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if the given value is not a function', (done) => {
-      const completable = Completable.fromResolvable();
+    completable.subscribe(
+      x => (x === 'Hello World' ? done() : done(false)),
+      e => done(e),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the given value is not a function', (done) => {
+    const completable = Completable.fromResolvable();
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
