b1c7033e3cd9b37bc87f2cdca85db6bd475a9ce9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 10:52:37 +0800

    Create emitter.js

---
+++ src/emitter.js
@@ -0,0 +1,25 @@
+/**
+ * @interface
+ * Represents an object that receives notification to
+ * an Observer.
+ *
+ * Emitter is an abstraction layer of the Observer
+ */
+// eslint-disable-next-line no-unused-vars
+export default class Emitter extends AbortController {
+  /**
+   * Emits a completion signal.
+   * @param {!any} value
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  onComplete() {}
+
+  /**
+   * Emits an error value.
+   * @param {!Error} err
+   * @abstract
+   */
+  // eslint-disable-next-line class-methods-use-this, no-unused-vars
+  onError(err) {}
+}
