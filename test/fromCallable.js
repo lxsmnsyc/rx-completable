91aacd5bf2a5e1aba36a482ca9b7090b31839711
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:14 +0800

    Update fromCallable.js

--- test/fromCallable.js
+++ test/fromCallable.js
@@ -5,63 +5,58 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#fromCallable', () => {
   /**
    *
    */
-  describe('#fromCallable', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.fromCallable(() => 'Hello World');
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should succeed with the given value.', (done) => {
-      const completable = Completable.fromCallable(() => 'Hello World');
+  it('should create a Completable', () => {
+    const completable = Completable.fromCallable(() => 'Hello World');
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should succeed with the given value.', (done) => {
+    const completable = Completable.fromCallable(() => 'Hello World');
 
-      completable.subscribe(
-        x => (x === 'Hello World' ? done() : done(false)),
-        e => done(e),
-      );
+    completable.subscribe(
+      x => (x === 'Hello World' ? done() : done(false)),
+      e => done(e),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if the callable throws an error.', (done) => {
+    const completable = Completable.fromCallable(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should signal an error if the callable throws an error.', (done) => {
-      const completable = Completable.fromCallable(() => {
-        throw new Error('Expected');
-      });
 
-      completable.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal an error if the callable returns a rejected Promise.', (done) => {
-      const completable = Completable.fromCallable(() => new Promise((x, y) => y(new Error('Expected'))));
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if the callable returns a rejected Promise.', (done) => {
+    const completable = Completable.fromCallable(() => new Promise((x, y) => y(new Error('Expected'))));
 
-      completable.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal an error if the callable is not a function', (done) => {
-      const completable = Completable.fromCallable();
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if the callable is not a function', (done) => {
+    const completable = Completable.fromCallable();
 
-      completable.subscribe(
-        () => done(false),
-        e => (e === 'Completable.fromCallable: callable received is not a function.' ? done() : done(false)),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      e => (e === 'Completable.fromCallable: callable received is not a function.' ? done() : done(false)),
+    );
   });
 });
