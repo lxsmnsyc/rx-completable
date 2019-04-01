1d77b9b8fdab3530c1a55d610f57aaf05f9d45bc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 10:07:18 +0800

    Update merge.js

--- test/merge.js
+++ test/merge.js
@@ -10,8 +10,51 @@ describe('#merge', () => {
    *
    */
   it('should create a Completable', () => {
-    const completable = Completable.merge(Completable.complete(), Completable.timer(100));
-
+    const completable = Completable.merge([Completable.complete(), Completable.complete()]);
     assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should start with the from the first completion to the last completion', (done) => {
+    let started = '';
+    const prefix = Completable.complete().doOnComplete(() => { started += 'A'; });
+    const suffix = Completable.complete().doOnComplete(() => { started += 'B'; });
+    const source = Completable.merge([prefix, suffix]);
+
+    source.subscribe(
+      () => (started === 'AB' ? done() : done(false)),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error from the first', (done) => {
+    const completable = Completable.merge([Completable.error(new Error('Hello')), Completable.timer(100)]);
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if one of the source is non-Completable.', (done) => {
+    const completable = Completable.merge(['Hello', Completable.timer(100)]);
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if given argument is not Iterable', (done) => {
+    const completable = Completable.merge();
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
