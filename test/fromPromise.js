8fd032f1af28726d1ed39e506dc5f82795306595
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:15 +0800

    Update fromPromise.js

--- test/fromPromise.js
+++ test/fromPromise.js
@@ -5,40 +5,34 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#fromPromise', () => {
   /**
    *
    */
-  describe('#fromPromise', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.fromPromise(new Promise(res => res('Hello World')));
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should succeed with the given value.', (done) => {
-      const completable = Completable.fromPromise(new Promise(res => res('Hello World')));
+  it('should create a Completable', () => {
+    const completable = Completable.fromPromise(new Promise(res => res('Hello World')));
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should succeed with the given value.', (done) => {
+    const completable = Completable.fromPromise(new Promise(res => res('Hello World')));
 
-      completable.subscribe(
-        x => (x === 'Hello World' ? done() : done(false)),
-        e => done(e),
-      );
-    });
+    completable.subscribe(
+      x => (x === 'Hello World' ? done() : done(false)),
+      e => done(e),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal error if the given value is not Promise like', (done) => {
+    const completable = Completable.fromPromise();
 
-    /**
-     *
-     */
-    it('should signal error if the given value is not Promise like', (done) => {
-      const completable = Completable.fromPromise();
-
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
