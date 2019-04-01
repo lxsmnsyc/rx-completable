7e9c243ab41a64766c7708de94eec5d674eebdfd
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:26:38 +0800

    Update compose.js

--- test/compose.js
+++ test/compose.js
@@ -3,51 +3,46 @@ import assert from 'assert';
 import Completable from '../src/completable';
 
 /**
- *
- */
-describe('Completable', () => {
-  /**
    *
    */
-  describe('#compose', () => {
-    /**
+describe('#compose', () => {
+  /**
      *
      */
-    it('should return the same instance if no function is provided', () => {
-      const source = Completable.complete();
-      const completable = source.compose();
+  it('should return the same instance if no function is provided', () => {
+    const source = Completable.complete();
+    const completable = source.compose();
 
-      assert(source === completable);
-    });
-    /**
+    assert(source === completable);
+  });
+  /**
      *
      */
-    it('should return a Completable from the transformer', () => {
-      const completable = Completable.complete().compose(source => source.map(x => `${x} World`));
+  it('should return a Completable from the transformer', () => {
+    const completable = Completable.complete().compose(source => source.delay(100));
 
-      assert(completable instanceof Completable);
-    });
-    /**
+    assert(completable instanceof Completable);
+  });
+  /**
      *
      */
-    it('should correctly signal the composed Completable', (done) => {
-      const completable = Completable.complete().compose(source => source.map(x => `${x} World`));
+  it('should correctly signal the composed Completable', (done) => {
+    const completable = Completable.complete().compose(source => source.delay(100));
 
-      completable.subscribe(
-        x => (x === 'Hello World' ? done() : done(x)),
-        done,
-      );
-    });
-    /**
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
      *
      */
-    it('should signal error if the transformer function returned a non-Completable', (done) => {
-      const completable = Completable.complete().compose(() => undefined);
+  it('should signal error if the transformer function returned a non-Completable', (done) => {
+    const completable = Completable.complete().compose(() => undefined);
 
-      completable.subscribe(
-        done,
-        () => done(),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
