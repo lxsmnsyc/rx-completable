7fed8a8f856d4f2fcd59a06bdbcd9affdcaf2787
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:01 +0800

    Update doOnError.js

--- test/doOnError.js
+++ test/doOnError.js
@@ -5,37 +5,32 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#doOnError', () => {
   /**
    *
    */
-  describe('#doOnError', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.complete().doOnError(() => {});
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if the method received a non-function parameter.', () => {
-      const source = Completable.complete();
-      const completable = source.doOnError();
-      assert(source === completable);
-    });
-    /**
-     *
-     */
-    it('should call the given function on error.', (done) => {
-      let called;
-      const source = Completable.error(new Error('Hello'));
-      const completable = source.doOnError(() => { called = true; });
-      completable.subscribe(
-        () => done(false),
-        () => called && done(),
-      );
-    });
+  it('should create a Completable', () => {
+    const completable = Completable.complete().doOnError(() => {});
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if the method received a non-function parameter.', () => {
+    const source = Completable.complete();
+    const completable = source.doOnError();
+    assert(source === completable);
+  });
+  /**
+   *
+   */
+  it('should call the given function on error.', (done) => {
+    let called;
+    const source = Completable.error(new Error('Hello'));
+    const completable = source.doOnError(() => { called = true; });
+    completable.subscribe(
+      () => done(false),
+      () => called && done(),
+    );
   });
 });
