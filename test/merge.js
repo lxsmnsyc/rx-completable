8afc65d53d077d490c7788ecdc989705779b2547
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:22 +0800

    Update merge.js

--- test/merge.js
+++ test/merge.js
@@ -5,51 +5,13 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#merge', () => {
   /**
    *
    */
-  describe('#merge', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.merge(Completable.just(Completable.complete()));
+  it('should create a Completable', () => {
+    const completable = Completable.merge(Completable.complete(), Completable.timer(100));
 
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should signal error if no source is provided', (done) => {
-      const completable = Completable.merge();
-
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal error if source emitted a non-Completable', (done) => {
-      const completable = Completable.merge(Completable.complete());
-
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal success of the signaled Completable', (done) => {
-      const completable = Completable.merge(Completable.just(Completable.complete()));
-
-      completable.subscribe(
-        x => (x === 'Hello' ? done() : done(false)),
-        () => done(false),
-      );
-    });
+    assert(completable instanceof Completable);
   });
 });
