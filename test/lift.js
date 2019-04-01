d1274cbf9eaf0a3b81f6c7ebfbdd08052167c68f
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:20 +0800

    Update lift.js

--- test/lift.js
+++ test/lift.js
@@ -4,43 +4,38 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#lift', () => {
   /**
    *
    */
-  describe('#lift', () => {
-    /**
-     *
-     */
-    it('should return the same instance if no function is provided', () => {
-      const source = Completable.complete();
-      const completable = source.lift();
+  it('should return the same instance if no function is provided', () => {
+    const source = Completable.complete();
+    const completable = source.lift();
 
-      assert(source === completable);
-    });
-    /**
-     *
-     */
-    it('should signal an error if the lift operator returned a non-Observer', (done) => {
-      const completable = Completable.complete().lift(() => {});
+    assert(source === completable);
+  });
+  /**
+   *
+   */
+  it('should signal an error if the lift operator returned a non-Observer', (done) => {
+    const completable = Completable.complete().lift(() => {});
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should subscribe successfully', (done) => {
-      const completable = Completable.complete().lift(observer => ({
-        onSubscribe: observer.onSubscribe,
-        onComplete: observer.onComplete,
-      }));
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should subscribe successfully', (done) => {
+    const completable = Completable.complete().lift(observer => ({
+      onSubscribe: observer.onSubscribe,
+      onComplete: observer.onComplete,
+    }));
 
-      completable.subscribe(
-        () => done(),
-      );
-    });
+    completable.subscribe(
+      () => done(),
+    );
   });
 });
