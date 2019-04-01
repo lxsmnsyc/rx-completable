23b1f8ee9eb7a0b0966ca075743b022ff238614e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:33 +0800

    Update retry.js

--- test/retry.js
+++ test/retry.js
@@ -5,57 +5,52 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#retry', () => {
   /**
    *
    */
-  describe('#retry', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.error(new Error('Hello')).retry(x => x === 3);
+  it('should create a Completable', () => {
+    const completable = Completable.error(new Error('Hello')).retry(x => x === 3);
 
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should signal a success if no error', (done) => {
-      const completable = Completable.complete().retry(x => x === 3);
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should signal a complete if no error', (done) => {
+    const completable = Completable.complete().retry(x => x === 3);
 
-      completable.subscribe(
-        () => done(),
-        () => done(false),
-      );
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should retry if there is an error and if it passes the predicate', (done) => {
+    let retried;
+    const completable = Completable.error(new Error('Hello')).retry((x) => {
+      if (x === 2) {
+        retried = true;
+      }
+      return x < 3;
     });
-    /**
-     *
-     */
-    it('should retry if there is an error and if it passes the predicate', (done) => {
-      let retried;
-      const completable = Completable.error(new Error('Hello')).retry((x) => {
-        if (x === 2) {
-          retried = true;
-        }
-        return x < 3;
-      });
 
-      completable.subscribe(
-        () => done(false),
-        () => retried && done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should signal an error if predicate is false', (done) => {
-      const completable = Completable.error(new Error('Hello')).retry(x => x === 3);
+    completable.subscribe(
+      () => done(false),
+      () => retried && done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should signal an error if predicate is false', (done) => {
+    const completable = Completable.error(new Error('Hello')).retry(x => x === 3);
 
-      completable.subscribe(
-        () => done(false),
-        () => done(),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
