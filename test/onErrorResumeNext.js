c7f0dfa7fe558dbd1fdd7a7e1b654087526e99f7
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:29 +0800

    Update onErrorResumeNext.js

--- test/onErrorResumeNext.js
+++ test/onErrorResumeNext.js
@@ -5,65 +5,60 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#onErrorResumeNext', () => {
   /**
    *
    */
-  describe('#onErrorResumeNext', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.error(new Error('Hello')).onErrorResumeNext(Completable.complete());
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should return the same instance if parameter passed is not a Completable or a function', () => {
-      const source = Completable.error(new Error('Hello'));
-      const completable = source.onErrorResumeNext();
-      assert(completable === source);
-    });
-    /**
-     *
-     */
-    it('should subscribe to the given Completable', (done) => {
-      const completable = Completable.error(new Error('Hello')).onErrorResumeNext(Completable.complete());
-      completable.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should subscribe to the given Completable-producing Function', (done) => {
-      const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => Completable.complete());
-      completable.subscribe(
-        () => done(),
-        done,
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function throws error.', (done) => {
-      const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => { throw new Error('Ooops'); });
-      completable.subscribe(
-        done,
-        () => done(),
-      );
-    });
-    /**
-     *
-     */
-    it('should emit error if provide function returns non-Completable.', (done) => {
-      const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => {});
-      completable.subscribe(
-        done,
-        () => done(),
-      );
-    });
+  it('should create a Completable', () => {
+    const completable = Completable.error(new Error('Hello')).onErrorResumeNext(Completable.complete());
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should return the same instance if parameter passed is not a Completable or a function', () => {
+    const source = Completable.error(new Error('Hello'));
+    const completable = source.onErrorResumeNext();
+    assert(completable === source);
+  });
+  /**
+   *
+   */
+  it('should subscribe to the given Completable', (done) => {
+    const completable = Completable.error(new Error('Hello')).onErrorResumeNext(Completable.complete());
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should subscribe to the given Completable-producing Function', (done) => {
+    const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => Completable.complete());
+    completable.subscribe(
+      () => done(),
+      () => done(false),
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function throws error.', (done) => {
+    const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => { throw new Error('Ooops'); });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
+  });
+  /**
+   *
+   */
+  it('should emit error if provide function returns non-Completable.', (done) => {
+    const completable = Completable.error(new Error('Hello')).onErrorResumeNext(() => {});
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
