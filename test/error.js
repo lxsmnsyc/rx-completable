cdcde1d2f366cdd806ed1c7264fb7abbff008956
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-01 09:27:11 +0800

    Update error.js

--- test/error.js
+++ test/error.js
@@ -5,64 +5,59 @@ import Completable from '../src/completable';
 /**
  *
  */
-describe('Completable', () => {
+describe('#error', () => {
   /**
    *
    */
-  describe('#error', () => {
-    /**
-     *
-     */
-    it('should create a Completable', () => {
-      const completable = Completable.error(new Error('Hello World'));
+  it('should create a Completable', () => {
+    const completable = Completable.error(new Error('Hello World'));
 
-      assert(completable instanceof Completable);
-    });
-    /**
-     *
-     */
-    it('should error with the given value.', (done) => {
-      const completable = Completable.error(new Error('Hello World'));
+    assert(completable instanceof Completable);
+  });
+  /**
+   *
+   */
+  it('should error with the given value.', (done) => {
+    const completable = Completable.error(new Error('Hello World'));
 
-      completable.subscribe(
-        () => done(false),
-        e => (typeof e !== 'undefined' ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should error with the a message if the given value is undefined', (done) => {
-      const completable = Completable.error();
+    completable.subscribe(
+      () => done(false),
+      e => (typeof e !== 'undefined' ? done() : done(false)),
+    );
+  });
+  /**
+   *
+   */
+  it('should error with the a message if the given value is undefined', (done) => {
+    const completable = Completable.error();
 
-      completable.subscribe(
-        () => done(false),
-        e => (e === 'Completable.error received an undefined value.' ? done() : done(false)),
-      );
-    });
-    /**
-     *
-     */
-    it('should error with the a message if the callable returned undefined', (done) => {
-      const completable = Completable.error(() => {});
+    completable.subscribe(
+      () => done(false),
+      e => (e === 'Completable.error received an undefined value.' ? done() : done(false)),
+    );
+  });
+  /**
+   *
+   */
+  it('should error with the a message if the callable returned undefined', (done) => {
+    const completable = Completable.error(() => {});
 
-      completable.subscribe(
-        () => done(false),
-        e => (e === 'Completable.error: Error supplier returned an undefined value.' ? done() : done(false)),
-      );
+    completable.subscribe(
+      () => done(false),
+      e => (e === 'Completable.error: Error supplier returned an undefined value.' ? done() : done(false)),
+    );
+  });
+  /**
+   *
+   */
+  it('should error if the callable throws an error.', (done) => {
+    const completable = Completable.error(() => {
+      throw new Error('Expected');
     });
-    /**
-     *
-     */
-    it('should error if the callable throws an error.', (done) => {
-      const completable = Completable.error(() => {
-        throw new Error('Expected');
-      });
 
-      completable.subscribe(
-        () => done(false),
-        e => (e instanceof Error ? done() : done(false)),
-      );
-    });
+    completable.subscribe(
+      () => done(false),
+      () => done(),
+    );
   });
 });
