fda31febd9230cba8c08be52e3dcca1384bbfc02
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-03 18:45:52 +0800

    Update never.js

--- src/internal/operators/never.js
+++ src/internal/operators/never.js
@@ -29,7 +29,7 @@ let INSTANCE;
  */
 export default () => {
   if (typeof INSTANCE === 'undefined') {
-    INSTANCE = new Completable();
+    INSTANCE = new Completable(subscribeActual);
     INSTANCE.subscribeActual = subscribeActual.bind(INSTANCE);
   }
   return INSTANCE;
