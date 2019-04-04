d7143fe8ddc8163496d67747d220ba0f53d356f9
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 14:11:18 +0800

    Update timeout.js

--- src/internal/operators/timeout.js
+++ src/internal/operators/timeout.js
@@ -1,7 +1,7 @@
 
 import AbortController from 'abort-controller';
 import Completable from '../../completable';
-import { cleanObserver } from '../utils';
+import { cleanObserver, isNumber } from '../utils';
 
 /**
  * @ignore
@@ -51,7 +51,7 @@ function subscribeActual(observer) {
  * @ignore
  */
 export default (source, amount) => {
-  if (typeof amount !== 'number') {
+  if (!isNumber(amount)) {
     return source;
   }
   const completable = new Completable(subscribeActual);
