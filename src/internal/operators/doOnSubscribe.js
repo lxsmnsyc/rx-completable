420b9d366f7f5e4bc6862cb39ae5a97e43dc4e8c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-04 14:10:51 +0800

    Update doOnSubscribe.js

--- src/internal/operators/doOnSubscribe.js
+++ src/internal/operators/doOnSubscribe.js
@@ -1,5 +1,5 @@
 import Completable from '../../completable';
-import { cleanObserver } from '../utils';
+import { cleanObserver, isFunction } from '../utils';
 
 /**
  * @ignore
@@ -23,7 +23,7 @@ function subscribeActual(observer) {
  * @ignore
  */
 export default (source, callable) => {
-  if (typeof callable !== 'function') {
+  if (!isFunction(callable)) {
     return source;
   }
   const completable = new Completable(subscribeActual);
