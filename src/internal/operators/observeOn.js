e7faf00c0457d85584bc24cd49bb3ac1df15f8c8
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-10 12:05:53 +0800

    Create observeOn.js

---
+++ src/internal/operators/observeOn.js
@@ -0,0 +1,50 @@
+import AbortController from 'abort-controller';
+import Scheduler from 'rx-scheduler';
+import Completable from '../../completable';
+import { cleanObserver } from '../utils';
+
+function subscribeActual(observer) {
+  const { onSubscribe, onComplete, onError } = cleanObserver(observer);
+
+  const { source, scheduler } = this;
+
+  const controller = new AbortController();
+  onSubscribe(controller);
+
+  const { signal } = controller;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  source.subscribeWith({
+    onSubscribe(ac) {
+      signal.addEventListener('abort', () => ac.abort());
+    },
+    onComplete() {
+      scheduler.schedule(() => {
+        onComplete();
+        controller.abort();
+      });
+    },
+    onError(x) {
+      scheduler.schedule(() => {
+        onError(x);
+        controller.abort();
+      });
+    },
+  });
+}
+/**
+ * @ignore
+ */
+export default (source, scheduler) => {
+  let sched = scheduler;
+  if (!(sched instanceof Scheduler.interface)) {
+    sched = Scheduler.current;
+  }
+  const completable = new Completable(subscribeActual);
+  completable.source = source;
+  completable.scheduler = sched;
+  return completable;
+};
