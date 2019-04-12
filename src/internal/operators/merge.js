50f23bf9b3749cec28b1aa2cd4a2bf2c31810fb3
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:15 +0800

    Update merge.js

--- src/internal/operators/merge.js
+++ src/internal/operators/merge.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-import AbortController from 'abort-controller';
+import { CompositeCancellable } from 'rx-cancellable';
 import Completable from '../../completable';
 import { isIterable, cleanObserver } from '../utils';
 import error from './error';
@@ -11,60 +11,44 @@ import error from './error';
 function subscribeActual(observer) {
   const { onComplete, onError, onSubscribe } = cleanObserver(observer);
 
-  const controller = new AbortController();
-
-  const { signal } = controller;
+  const controller = new CompositeCancellable();
 
   onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
 
   const { sources } = this;
 
   const buffer = [];
   // eslint-disable-next-line no-restricted-syntax
   for (const completable of sources) {
-    if (signal.aborted) {
-      return;
-    }
     if (completable instanceof Completable) {
       buffer.unshift(completable);
     } else {
       onError(new Error('Completable.amb: One of the sources is a non-Completable.'));
-      controller.abort();
-      break;
+      controller.cancel();
+      return;
     }
   }
 
-  if (signal.aborted) {
-    return;
-  }
-
   let pending = buffer.length;
-  let current;
   for (const completable of buffer) {
     completable.subscribeWith({
       onSubscribe(ac) {
-        signal.addEventListener('abort', () => ac.abort());
+        controller.add(ac);
       },
       onComplete() {
         pending -= 1;
 
         if (pending === 0) {
           onComplete();
-          controller.abort();
+          controller.cancel();
         }
       },
       onError(x) {
         onError(x);
-        controller.abort();
+        controller.cancel();
       },
     });
   }
-
-  current();
 }
 /**
  * @ignore
