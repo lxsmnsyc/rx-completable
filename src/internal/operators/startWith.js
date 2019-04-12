e1ef7f8c5e3095e070bcd3f4ab60b88c5ce4d120
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:31 +0800

    Update startWith.js

--- src/internal/operators/startWith.js
+++ src/internal/operators/startWith.js
@@ -1,50 +1,5 @@
-import AbortController from 'abort-controller';
-import { cleanObserver } from '../utils';
+import concat from './concat';
 import Completable from '../../completable';
-
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const { onComplete, onError, onSubscribe } = cleanObserver(observer);
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-  const { source, other } = this;
-
-  other.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onComplete() {
-      source.subscribeWith({
-        onSubscribe(ac) {
-          signal.addEventListener('abort', () => ac.abort());
-        },
-        onComplete() {
-          onComplete();
-          controller.abort();
-        },
-        onError(x) {
-          onError(x);
-          controller.abort();
-        },
-      });
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-}
 
 /**
  * @ignore
@@ -53,8 +8,5 @@ export default (source, other) => {
   if (!(other instanceof Completable)) {
     return source;
   }
-  const completable = new Completable(subscribeActual);
-  completable.source = source;
-  completable.other = other;
-  return completable;
+  return concat([other, source]);
 };
