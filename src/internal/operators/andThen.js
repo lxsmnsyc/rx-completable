b2cb1046d79b5b9fb090db36a7053b5d9e01fede
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:24:51 +0800

    Update andThen.js

--- src/internal/operators/andThen.js
+++ src/internal/operators/andThen.js
@@ -1,51 +1,5 @@
-import AbortController from 'abort-controller';
-import { cleanObserver } from '../utils';
 import Completable from '../../completable';
-
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const { onComplete, onError, onSubscribe } = cleanObserver(observer);
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-  const { source, other } = this;
-
-  source.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onComplete() {
-      other.subscribeWith({
-        onSubscribe(ac) {
-          signal.addEventListener('abort', () => ac.abort());
-        },
-        onComplete() {
-          onComplete();
-          controller.abort();
-        },
-        onError(x) {
-          onError(x);
-          controller.abort();
-        },
-      });
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-}
-
+import concat from './concat';
 /**
  * @ignore
  */
@@ -53,8 +7,5 @@ export default (source, other) => {
   if (!(other instanceof Completable)) {
     return source;
   }
-  const completable = new Completable(subscribeActual);
-  completable.source = source;
-  completable.other = other;
-  return completable;
+  return concat([source, other]);
 };
