547bb09c52e8705c9df9a1bcf8595ab9faf510df
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:24:49 +0800

    Update ambWith.js

--- src/internal/operators/ambWith.js
+++ src/internal/operators/ambWith.js
@@ -1,57 +1,5 @@
-import AbortController from 'abort-controller';
 import Completable from '../../completable';
-import { cleanObserver } from '../utils';
-
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const { onComplete, onError, onSubscribe } = cleanObserver(observer);
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-  const sharedComplete = () => {
-    if (!signal.aborted) {
-      onComplete();
-      controller.abort();
-    }
-  };
-  const sharedError = (x) => {
-    if (!signal.aborted) {
-      onError(x);
-      controller.abort();
-    }
-  };
-
-  const { source, other } = this;
-
-  source.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onComplete: sharedComplete,
-    onError: sharedError,
-  });
-  other.subscribeWith({
-    onSubscribe(ac) {
-      if (signal.aborted) {
-        ac.abort();
-      } else {
-        signal.addEventListener('abort', () => ac.abort());
-      }
-    },
-    onComplete: sharedComplete,
-    onError: sharedError,
-  });
-}
+import amb from './amb';
 /**
  * @ignore
  */
@@ -59,8 +7,5 @@ export default (source, other) => {
   if (!(other instanceof Completable)) {
     return source;
   }
-  const completable = new Completable(subscribeActual);
-  completable.source = source;
-  completable.other = other;
-  return completable;
+  return amb([source, other]);
 };
