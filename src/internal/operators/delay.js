8b0215a4e32259ff2ebb4aaaee73553d17e98952
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:00 +0800

    Update delay.js

--- src/internal/operators/delay.js
+++ src/internal/operators/delay.js
@@ -1,5 +1,5 @@
-import AbortController from 'abort-controller';
 import Scheduler from 'rx-scheduler';
+import { LinkedCancellable } from 'rx-cancellable';
 import Completable from '../../completable';
 import { cleanObserver, isNumber } from '../utils';
 
@@ -11,41 +11,23 @@ function subscribeActual(observer) {
 
   const { amount, scheduler, doDelayError } = this;
 
-  const controller = new AbortController();
-
-  const { signal } = controller;
+  const controller = new LinkedCancellable();
 
   onSubscribe(controller);
 
-  if (signal.aborted) {
-    return;
-  }
-
   this.source.subscribeWith({
     onSubscribe(ac) {
-      signal.addEventListener('abort', () => {
-        ac.abort();
-      });
+      controller.link(ac);
     },
     onComplete() {
-      const ac = scheduler.delay(() => {
+      controller.link(scheduler.delay(() => {
         onComplete();
-        controller.abort();
-      }, amount);
-
-      signal.addEventListener('abort', () => {
-        ac.abort();
-      });
+      }, amount));
     },
     onError(x) {
-      const ac = scheduler.delay(() => {
+      controller.link(scheduler.delay(() => {
         onError(x);
-        controller.abort();
-      }, doDelayError ? amount : 0);
-
-      signal.addEventListener('abort', () => {
-        ac.abort();
-      });
+      }, doDelayError ? amount : 0));
     },
   });
 }
