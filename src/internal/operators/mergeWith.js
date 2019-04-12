f79b20def078de210008c1c2e289fdb8adb749b5
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:17 +0800

    Update mergeWith.js

--- src/internal/operators/mergeWith.js
+++ src/internal/operators/mergeWith.js
@@ -1,69 +1,5 @@
-import AbortController from 'abort-controller';
-import { cleanObserver } from '../utils';
 import Completable from '../../completable';
-
-/**
- * @ignore
- */
-function subscribeActual(observer) {
-  const { onComplete, onError, onSubscribe } = cleanObserver(observer);
-
-  const controller = new AbortController();
-
-  const { signal } = controller;
-
-  onSubscribe(controller);
-
-  if (signal.aborted) {
-    return;
-  }
-
-  const { source, other } = this;
-
-  let flagA = false;
-  let flagB = false;
-
-  source.subscribeWith({
-    onSubscribe(ac) {
-      signal.addEventListener('abort', () => ac.abort());
-    },
-    onComplete() {
-      flagA = true;
-
-      if (flagB) {
-        onComplete();
-        controller.abort();
-      }
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-
-  other.subscribeWith({
-    onSubscribe(ac) {
-      if (signal.aborted) {
-        ac.abort();
-      } else {
-        signal.addEventListener('abort', () => ac.abort());
-      }
-    },
-    onComplete() {
-      flagB = true;
-
-      if (flagA) {
-        onComplete();
-        controller.abort();
-      }
-    },
-    onError(x) {
-      onError(x);
-      controller.abort();
-    },
-  });
-}
-
+import merge from './merge';
 /**
  * @ignore
  */
@@ -71,8 +7,5 @@ export default (source, other) => {
   if (!(other instanceof Completable)) {
     return source;
   }
-  const completable = new Completable(subscribeActual);
-  completable.source = source;
-  completable.other = other;
-  return completable;
+  return merge([source, other]);
 };
