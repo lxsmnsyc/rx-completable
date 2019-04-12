0070394b06772b5b5e570fee23bbf8d247c879fe
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:41 +0800

    Update observer.js

--- src/observer.js
+++ src/observer.js
@@ -1,13 +1,31 @@
 /**
+ * Provides a mechanism for receiving push-based notification of a valueless
+ * completion or an error.
+ *
+ * When a CompletableObserver is subscribed to a Completable through the
+ * Completable.subscribe(CompletableObserver) method, the Completable calls
+ * onSubscribe(Cancellable) with a Cancellable that allows cancelling the
+ * sequence at any time. A well-behaved Completable will call a CompletableObserver's
+ * onError(Error) or onComplete() method exactly once as they are considered
+ * mutually exclusive terminal signals.
+ *
+ * the invocation pattern must adhere to the following protocol:
+ *
+ * <code>onSubscribe (onError | onComplete)?</code>
+ *
+ * Subscribing a CompletableObserver to multiple Completables is not recommended.
+ * If such reuse happens, it is the duty of the CompletableObserver implementation
+ * to be ready to receive multiple calls to its methods and ensure proper concurrent
+ * behavior of its business logic.
+ *
+ * Calling onSubscribe(Cancellable) or onError(Error) with a null argument is forbidden.
  * @interface
- * Represents an object that receives notification from
- * an Emitter.
  */
 // eslint-disable-next-line no-unused-vars
-export default class Observer {
+export default class CompletableObserver {
   /**
-   * Receives the AbortController subscription.
-   * @param {!AbortController} d
+   * Receives the Cancellable subscription.
+   * @param {!Cancellable} d
    * @abstract
    */
   // eslint-disable-next-line class-methods-use-this, no-unused-vars
