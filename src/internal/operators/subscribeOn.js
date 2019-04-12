d4ea0e32a7089fa6203db3bd68f3bdced41ed54e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-12 13:25:32 +0800

    Update subscribeOn.js

--- src/internal/operators/subscribeOn.js
+++ src/internal/operators/subscribeOn.js
@@ -1,5 +1,5 @@
-import AbortController from 'abort-controller';
 import Scheduler from 'rx-scheduler';
+import { LinkedCancellable } from 'rx-cancellable';
 import Completable from '../../completable';
 import { cleanObserver } from '../utils';
 
@@ -8,33 +8,20 @@ function subscribeActual(observer) {
 
   const { source, scheduler } = this;
 
-  const controller = new AbortController();
+  const controller = new LinkedCancellable();
+
   onSubscribe(controller);
 
-  const { signal } = controller;
-
-  if (signal.aborted) {
-    return;
-  }
-
-  scheduler.schedule(() => {
-    if (signal.aborted) {
-      return;
-    }
+  controller.link(scheduler.schedule(() => {
+    controller.unlink();
     source.subscribeWith({
       onSubscribe(ac) {
-        signal.addEventListener('abort', () => ac.abort());
+        controller.link(ac);
       },
-      onComplete() {
-        onComplete();
-        controller.abort();
-      },
-      onError(x) {
-        onError(x);
-        controller.abort();
-      },
+      onComplete,
+      onError,
     });
-  });
+  }));
 }
 /**
  * @ignore
