e245b9f5a8cc6d9dc4fa9d5174959994bfc2bae6
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-17 11:50:14 +0800

    Update compose.js

--- src/internal/operators/compose.js
+++ src/internal/operators/compose.js
@@ -1,6 +1,6 @@
-import Completable from '../../completable';
 import error from './error';
 import { isFunction } from '../utils';
+import is from '../is';
 
 /**
  * @ignore
@@ -15,7 +15,7 @@ export default (source, transformer) => {
   try {
     result = transformer(source);
 
-    if (!(result instanceof Completable)) {
+    if (!is(result)) {
       throw new Error('Completable.compose: transformer returned a non-Completable.');
     }
   } catch (e) {
