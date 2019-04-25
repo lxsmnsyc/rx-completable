773ceeb2deee47e9fc5bea0d5b8466ab45ab3871
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-04-25 16:18:34 +0800

    Update retry.js

--- src/internal/operators/retry.js
+++ src/internal/operators/retry.js
@@ -33,7 +33,6 @@ function subscribeActual(observer) {
             sub();
           } else {
             onError(x);
-            controller.cancel();
           }
         } else {
           sub();
